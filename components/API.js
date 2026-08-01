ff69884b3de724748963b675043681cbefd4a7e8
= <=>
2026-08-01 14:49:31 +0530

    fix: Refactor API base URL assignment for improved readability and logging

--- components/API.js
+++ components/API.js
@@ -1,2 +1,6 @@
-const api = `${process.env.NEXT_PUBLIC_API_URL}/api/auth/`;
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
+console.log('Current API Base URL:', API_BASE_URL);
+
+const api = `${API_BASE_URL}/api/auth/`;
 export default api;
