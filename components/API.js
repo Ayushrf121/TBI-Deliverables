be275ab1c668cdb9a17500d792d823982fc72ff1
= <=>
2026-08-01 14:51:53 +0530

    fix: Remove console log for API base URL to clean up code

--- components/API.js
+++ components/API.js
@@ -1,6 +1,3 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
-
-console.log('Current API Base URL:', API_BASE_URL);
-
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL
 const api = `${API_BASE_URL}/api/auth/`;
 export default api;
